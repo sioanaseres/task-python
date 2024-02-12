8c34bfd8093d613b31d0276deb2749e752c38ec3
Ioana Cosoreanu-Seres <ioana.cosoreanuseres@888holdings.com>
2024-02-12 12:29:03 +0200

    railway updates

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY', default='default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -124,8 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, "static"))]
+STATIC_ROOT  = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
